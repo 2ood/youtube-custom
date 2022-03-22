05727d41e46a85227f27751f1cc8ee5153f30198
2ood <kyungmin.official0@gmail.com>
2022-03-23 01:30:59 +0900

    v1.0.0

--- content.js
+++ content.js
@@ -15,15 +15,22 @@ function buildButton() {
   let result = document.createElement("div");
   let img = document.createElement("img");
   const path = chrome.runtime.getURL("mirror.svg");
-  
+
   img.id="mirror-image";
   img.src=path;
+  img.style.opacity ="0.5";
 
   result.appendChild(img);
   result.id = "mirror-button";
   result.addEventListener("click",()=>{
-    if(video.style.transform!=transform) video.style.transform = transform;
-    else video.style.transform="";
+    if(video.style.transform!=transform) {
+      video.style.transform = transform;
+      img.style.opacity = "1";
+    }
+    else {
+      video.style.transform="";
+      img.style.opacity ="0.5";
+    }
   });
 
   return result;
