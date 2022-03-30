48fb364707982697cf6795e1fbbea115013bbe6b
2ood <kyungmin.official0@gmail.com>
2022-03-30 16:54:39 +0900

    discontinuing this project; will be back when I'm ready

--- mytube/list.js
+++ mytube/list.js
@@ -2,14 +2,14 @@ const params = new URLSearchParams(window.location.search);
 
 query = params.get("q");
 
-fetch(`https://www.youtube.com/results?search_query=${query}`,{
+let response =  fetch(`https://www.youtube.com/results?search_query=${query}`,{
     method: 'GET',
     mode: 'no-cors', // no-cors, *cors, same-origin
     cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'omit', // include, *same-origin, omit
-    }).then((resp)=>{
-        console.log(resp);
-        resp.text().then((data) => {
-            console.log("data:" + data);
-    });
-  });
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'text/html'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    }}).then()
+
+console.log(response);
